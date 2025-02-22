bc83b08c2874b4a837ac421f13aa53cc8d07c839
Mohit V S <vsmohit94@gmail.com>
2025-02-22 15:40:06 +0530

    added testimonials and features sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,21 @@
+import FeatureCard from "./native-components/FeatureCard/FeatureCard";
+import Testimonial from "./native-components/Testimonial/Testimonial";
 
 export default function Home() {
-    return <p>Hello, world!</p>;
+    return (
+        <div>
+            {/* Hero section */}
+
+
+            {/* Features section */}
+            <section id="features" className="w-full">
+                <FeatureCard />
+            </section>
+
+            {/* Testimonial Marquee */}
+            <section id="testimaonial-marquee">
+                <Testimonial />
+            </section>
+        </div>
+    );
 }
