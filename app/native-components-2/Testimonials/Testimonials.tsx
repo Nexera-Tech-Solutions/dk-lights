d11ea78d05ee7f21c04f51b6eb25a61b5d9f4269
Mohit V S <vsmohit94@gmail.com>
2025-02-23 16:32:59 +0530

    added profile icon to the testimonial

--- app/native-components-2/Testimonials/Testimonials.tsx
+++ app/native-components-2/Testimonials/Testimonials.tsx
@@ -9,6 +9,7 @@ import React from "react";
 import { TESTIMONIALS } from "./data";
 import { range } from "@/lib/utils";
 import { Star } from "lucide-react";
+import Image from "next/image";
 
 const Testimonials = () => {
     return (
@@ -21,15 +22,26 @@ const Testimonials = () => {
                 {TESTIMONIALS.map((test, index) => (
                     <Card key={index} className="border-none shadow-none">
                         <CardHeader>
-                            <CardTitle className="mb-5 text-lg">{test.name}</CardTitle>
-                            <CardDescription className="tracking-wide">{test.body}</CardDescription>
+                            <CardTitle className="mb-5 text-lg flex flex-row gap-2">
+                                <Image
+                                    src="/global/random-user-image-1.jpg"
+                                    alt="user=poflie-icon"
+                                    height={28}
+                                    width={28}
+                                    className="rounded-full"
+                                />
+                                {test.name}
+                            </CardTitle>
+                            <CardDescription className="tracking-wide">
+                                {test.body}
+                            </CardDescription>
                         </CardHeader>
                         <CardFooter>
                             {range(test.stars).map((val) => (
-                                <Star size={24} fill="black" key={val}/>
+                                <Star size={24} fill="black" key={val} />
                             ))}
                             {range(5 - test.stars).map((val) => (
-                                <Star size={24} key={val}/>
+                                <Star size={24} key={val} />
                             ))}
                         </CardFooter>
                     </Card>
