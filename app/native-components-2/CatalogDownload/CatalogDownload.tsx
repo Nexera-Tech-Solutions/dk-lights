7a9518a0069861b632ae47130ac6f2b443b6f6a0
arjun br <arjunbr@arjuns-MacBook-Air.local>
2025-02-23 18:32:20 +0530

    fix:ui of downloadcatalog page

--- app/native-components-2/CatalogDownload/CatalogDownload.tsx
+++ app/native-components-2/CatalogDownload/CatalogDownload.tsx
@@ -3,50 +3,38 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
 import { InteractiveHoverButton } from "@/components/magicui/interactive-hover-button";
-import useTextAnnotation from "@/app/hooks/useTextAnnotation";
 
 const CatalogDownload = () => {
-  useTextAnnotation("catalog-text");
-
   return (
-    <section className="flex flex-col justify-center items-center pb-10">
-      <h2 className="w-fit pl-4 mx-auto text-2xl md:text-4xl font-bold text-black my-5">
-        Download
-        <span id="catalog-text" className="mx-5">
-          Our Catalog
-        </span>
-      </h2>
-      <div>
-        <motion.div
-          className="bg-white rounded-lg shadow-xl p-10 flex items-center justify-between gap-16 "
-          whileHover={{
-            scale: 1.05,
-            transition: { duration: 0.3, ease: "easeOut" },
-          }}
-        >
-          <div className="flex items-center justify-center">
-            <Image
-              src="/global/random-image-2.jpg"
-              alt={"icon-img"}
-              width={430}
-              height={430}
-            />
+    <section className="flex justify-center pb-10">
+      <motion.div className="bg-white rounded-sm shadow-md p-5 flex items-center justify-between gap-16 max-w-[1000px] my-10">
+        <div className="flex items-center justify-center flex-1">
+          <Image
+            src="/global/random-image-2.jpg"
+            alt={"icon-img"}
+            width={430}
+            height={430}
+            className="rounded-sm shadow-inner"
+          />
+        </div>
+        <div className="text-center mt-4 flex-1">
+          <h2 className="text-3xl font-bold pb-2">
+            Discover the Latest Trends
+          </h2>
+          <h2 className="text-base ">
+            Explore our complete range of premium lighting solutions in one
+            place. Download our latest catalogue to discover innovative designs,
+            energy-efficient technology, and custom lighting options tailored
+            for your space.
+          </h2>
+          <div className="flex justify-center">
+            <InteractiveHoverButton className="my-10 rounded-sm">
+              {" "}
+              Download Catalog
+            </InteractiveHoverButton>
           </div>
-          <div className="text-center mt-4">
-            <h2 className="text-3xl font-bold pb-2">
-              Discover the Latest Trends
-            </h2>
-            <h2 className="text-base ">
-              Download now and transform your space!
-            </h2>
-            <div className="flex justify-center">
-              <InteractiveHoverButton className="my-10 rounded-sm">
-                Download Catalog
-              </InteractiveHoverButton>
-            </div>
-          </div>
-        </motion.div>
-      </div>
+        </div>
+      </motion.div>
     </section>
   );
 };
